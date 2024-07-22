f2c1efbebcc27a04ffcd9da3207d3cf0e8ee6b81
Ben Reid <reidben24@gmail.com>
2024-07-22 00:00:02 -0400

    Add prefix and url encode options

--- m3u-gen.py
+++ m3u-gen.py
@@ -1,8 +1,14 @@
 import os
 import argparse
+import urllib.parse
 
 
-def create_m3u_playlist(input_dir, output_dir):
+def path_to_file_uri(path):
+    """Convert absolute path to file URI."""
+    return urllib.parse.quote(path)
+
+
+def create_m3u_playlist(input_dir, output_dir, rel_dir, prefix, uri_encode):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -17,15 +23,21 @@ def create_m3u_playlist(input_dir, output_dir):
         with open(m3u_path, 'w', encoding='utf-8') as m3u_file:
             for music_file in music_files:
                 full_music_path = os.path.join(folder_path, music_file)
-                rel_path = os.path.relpath(full_music_path, output_dir)
-                m3u_file.write(f'local.track:{rel_path}\n')
+                rel_path = os.path.relpath(full_music_path, rel_dir)
+                if uri_encode:
+                    rel_path = path_to_file_uri(rel_path)
+                m3u_file.write(f'{prefix}{rel_path}\n')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate M3U playlists for each first level child folder in the parent directory")
     parser.add_argument("-i", "--input_directory", help="Path to the top-level input directory")
     parser.add_argument("-o", "--output_directory", help="Path to the output directory for M3U playlists")
+    parser.add_argument("-r", "--relative_to", default="", help="Path to compute relative paths from. Defaults to output_directory")
+    parser.add_argument("-p", "--prefix", default="", help="Optional prefix to apply to file paths in the output m3u")
+    parser.add_argument("-e", "--uri_encode", default=False, help="Whether to URI encode the output paths or not")
     args = parser.parse_args()
 
-    create_m3u_playlist(args.input_directory, args.output_directory)
+    rp = args.relative_to if args.relative_to else args.output_directory
+    create_m3u_playlist(args.input_directory, args.output_directory, rp, args.prefix, args.uri_encode)
 
